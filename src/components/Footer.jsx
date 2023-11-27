1e860891a9901e7ba7d7ad825eb1cfe635bbe914
iiMaInUsErii <119872193+iiMaInUsErii@users.noreply.github.com>
2023-11-27 22:37:01 +0300

    3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                     alt="Buy Me a Coffee Widget"
                     src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQJa8mAi-jjkiH6ojJ-1Rj6m1DV_vKxynr4hMEpoi4yjK1ILsZQ"
                 />
-            </a>
+            </div>
         </div>
     )
 }
